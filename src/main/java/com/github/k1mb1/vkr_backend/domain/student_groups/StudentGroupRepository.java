08add054a6e1fe25c55f0060d7e2fc5cd7fc5a31
k1mb1 <k1mb.redkin@gmail.com>
2026-03-31 14:08:30 +0300

    feat: add bulk insert for subgroups and optimize group creation logic

--- src/main/java/com/github/k1mb1/vkr_backend/domain/student_groups/StudentGroupRepository.java
+++ src/main/java/com/github/k1mb1/vkr_backend/domain/student_groups/StudentGroupRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -30,6 +31,7 @@ public interface StudentGroupRepository
         value = """
         SELECT new com.github.k1mb1.vkr_backend.domain.student_groups.responses.StudentGroupPageResponse(
             g.id, g.name,
+            (SELECT COUNT(s) FROM StudentEntity s WHERE s.group = g OR s.group.parentGroup = g),
             (SELECT COUNT(sg) FROM StudentGroupEntity sg WHERE sg.parentGroup = g)
         )
         FROM StudentGroupEntity g
@@ -52,5 +54,20 @@ public interface StudentGroupRepository
     )
     Optional<StudentGroupEntity> findWithSubgroupsAndStudentsById(
         @Param("id") UUID id
+    );
+
+    @Modifying
+    @Query(
+        value = """
+        INSERT INTO student_groups (created_at, name, parent_group_id, updated_at)
+        SELECT now(), CONCAT(:groupName, '/', gs), :parentGroupId, now()
+        FROM generate_series(1, :subgroupCount) gs
+        """,
+        nativeQuery = true
+    )
+    void insertSubgroupsBulk(
+        @Param("parentGroupId") UUID parentGroupId,
+        @Param("groupName") String groupName,
+        @Param("subgroupCount") int subgroupCount
     );
 }
